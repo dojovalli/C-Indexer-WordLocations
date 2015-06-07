05f7c13be8cb95925ad79b66574fa5ea1645b8ac
Matthew Valli <matthew.valli@gmail.com>
2015-06-07 13:45:05 -0700

    Initial Commit

---
+++ advC_lab_5/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  advC_lab_5
+//
+//  Created by Matthew Valli on 6/7/15.
+//  Copyright (c) 2015 Matthew Valli. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
